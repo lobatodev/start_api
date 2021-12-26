cc7b3f1580dad2d7b9123d2293220298d0214288
Marcus Lobato <79845572+marcuslobatoo@users.noreply.github.com>
2021-12-26 14:14:46 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 module.exports = {
-  dialect: process.env.DB_DIALECT_SCHOOL,
-  host: process.env.DB_HOST_SCHOOL,
-  username: process.env.DB_USER_SCHOOL,
-  port: process.env.DB_PORT_SCHOOL,
-  password: process.env.DB_PASS_SCHOOL,
-  database: process.env.DB_DATABASE_SCHOOL,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
